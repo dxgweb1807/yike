332be6861911c14d8291e27a10e1a4fb6f444c78
deng <291326520@qq.com>
2018-11-19 15:55:09 +0800

    第一个功能，点击按钮显示隐藏效果

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,39 @@
 /*
 * @Author: Administrator
 * @Date:   2018-11-15 16:49:22
-* @Last Modified by:   Administrator
-* @Last Modified time: 2018-11-15 16:49:29
+* @Last Modified by:   deng
+* @Last Modified time: 2018-11-19 15:46:46
 */
+var yike =angular.module('yike',[]);
+yike.run(["$rootScope",function($rootScope){
+       //alert("111")
+       //给头部的a 标签绑定toggle方法
+       $rootScope.left = false;
+       $rootScope.toggle = function(){
+       	     if( $rootScope.left ==true){
+       	     	$rootScope.left = false;
+       	     }else{
+       	     	$rootScope.left =true;
+       	     }
+       	   var dd = document.querySelectorAll("dd");
+       	   if($rootScope.left){
+                 for(var i=0; i<dd.length; i++) {
+			dd[i].style.transitionDuration = (i + 1) * 0.15 + 's';
+			dd[i].style.transitionProperty = 'all';
+			dd[i].style.transitionDelay = '0.2s';
+			dd[i].style.transitionTimingFunction = 'ease-out';
+			dd[i].style.transform = 'translate(0)';
+		     }
+       	   }else{
+                   for(var i=dd.length - 1; i>=0; i--) {
+			dd[i].style.transitionDuration = (dd.length - i + 1) * 0.05 + 's';
+			dd[i].style.transitionProperty = 'all';
+			dd[i].style.transitionDelay = '';
+			dd[i].style.transitionTimingFunction = 'ease-out';
+			dd[i].style.transform = 'translate(-100%)';
+		     }  
+       	   }
+       }
+
+
+}]);
